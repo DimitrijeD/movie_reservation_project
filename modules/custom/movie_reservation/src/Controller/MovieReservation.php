1851ccb8ab71e8fec6aab42512202cb9d61d57fb
DimitrijeD <dimitrijedrakulic@gmail.com>
2021-07-13 14:15:27 +0200

    no more comments

--- modules/custom/movie_reservation/src/Controller/MovieReservation.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservation.php
@@ -5,9 +5,6 @@ use Drupal\Core\Controller\ControllerBase;
 
 class MovieReservation extends ControllerBase {
   public function content(){
-//    $pageTitle = [
-//      'title' => 'Welcome to our movie reservation page',
-//    ];
 
     return [
       '#theme' => 'movie-reservation',
