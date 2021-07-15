3c94de473deae7ac5e6eba900eda4b093c8538d7
DimitrijeD <dimitrijedrakulic@gmail.com>
2021-07-15 11:43:23 +0200

    moved logic in yml

--- modules/custom/all_movies_listing/src/Controller/AllMoviesListing.php
+++ modules/custom/all_movies_listing/src/Controller/AllMoviesListing.php
@@ -2,25 +2,13 @@
 namespace Drupal\all_movies_listing\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Database\Database;
 use Drupal\node\Entity\Node;
-use Drupal\media\Entity\Media;
-use Drupal\file\Entity\File;
 
 class AllMoviesListing extends ControllerBase{
   public function content(){
 
     $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->execute();
-    $nodes = Node::loadMultiple($allNodeIds);
-
-    $movies = array();
-    $key = 1;
-    foreach ($nodes as $node){
-      $movies[$key]['title'] = $node->get('title')->value;
-      $movies[$key]['image_uri'] = $node->field_image_field->entity->getFileUri();
-      $movies[$key]['description'] = $node->get('field_description')->value;
-      $key++;
-    }
+    $movies = Node::loadMultiple($allNodeIds);
 
     return [
       '#theme' => 'all-movies-listing',
