44fab9cef003071e9628b0db110198fb2642543a
DimitrijeD <dimitrijedrakulic@gmail.com>
2021-07-19 17:12:04 +0200

    #3-Setup-twig-debug

--- modules/custom/all_movies_listing/src/Controller/AllMoviesListing.php
+++ modules/custom/all_movies_listing/src/Controller/AllMoviesListing.php
@@ -10,16 +10,6 @@ class AllMoviesListing extends ControllerBase{
     $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->execute();
     $movies = Node::loadMultiple($allNodeIds);
 
-//    $movie_categories = $movies[  3]->field_category; //->value
-//    $vid = 'movie_type';
-//    $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid);
-//    foreach ($terms as $term) {
-//      $term_data[] = array(
-//        'id' => $term->tid,
-//        'name' => $term->name
-//      );
-//    }
-
     return [
       '#theme' => 'all-movies-listing',
       '#pageTitle' => 'A List of all movies',
